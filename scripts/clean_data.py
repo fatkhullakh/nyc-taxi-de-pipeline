4a707579c087ffaeb13463c71f4718ac9e75c373
fatkhullakh <tfatkh@gmail.com>
2025-05-15 23:43:33 +0200

    Just a simple ingesting and cleaning data type parquet

---
+++ scripts/clean_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+parquet_path = 'data/raw/yellow_tripdata_2021-01.parquet'
+output_path = 'data/clean/yellow_tripdata_2021-01-cleaned.parquet'
+
+df = pd.read_parquet(parquet_path)
+
+df = df.dropna(subset=['passenger_count', 'RatecodeID', 'store_and_fwd_flag', 'congestion_surcharge',])
+
+df = df.drop(columns=['airport_fee'])
+
+df = df[
+    (df['trip_distance'] > 0) &
+    (df['fare_amount'] > 0) &
+    (df['passenger_count'] > 0)
+]
+
+df.to_parquet(output_path, index=False)
+
+
